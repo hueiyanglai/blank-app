21f952fc41b77ac8749e03c6c8ecd7f85ac725ec
hueiyanglai <hueiyanglai@gmail.com>
2024-10-14 00:03:58 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
-
-st.title("🎈 My new app wow")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+import pandas as pd
+ 
+st.write("""
+# My first app
+Hello *world!*
+""")
